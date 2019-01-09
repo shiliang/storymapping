1371004853f52eb081ebcfe9be52b55039e3ebe3
steven <shi_liang_china@163.com>
2019-01-09 17:27:23 +0800

    Initial commit

---
+++ src/main/java/com/nju/storymapping/StorymappingApplication.java
@@ -0,0 +1,14 @@
+package com.nju.storymapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorymappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorymappingApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/nju/storymapping/StorymappingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nju.storymapping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StorymappingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
