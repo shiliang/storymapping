99ba3d8f6bfaafe068dab8ede688caadfe9fa140
steven <shi_liang_china@163.com>
2019-01-25 10:17:12 +0800

    打包成war包修改代码

--- src/main/java/com/nju/storymapping/StorymappingApplication.java
+++ src/main/java/com/nju/storymapping/StorymappingApplication.java
@@ -3,14 +3,24 @@ package com.nju.storymapping;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.niu.storymapping.mapper")
-public class StorymappingApplication {
+public class StorymappingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StorymappingApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        builder.sources(this.getClass());
+        return super.configure(builder);
+    }
+
 
 
 
