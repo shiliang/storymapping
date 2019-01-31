ee76e8b7390ebc9836161bbfb99174d62598f3cc
ZhangDaJian1994 <jiafeimao.abc@gmail.com>
2019-01-31 20:59:39 +0800

    加入仪表盘

--- src/main/java/com/nju/storymapping/controller/CardController.java
+++ src/main/java/com/nju/storymapping/controller/CardController.java
@@ -6,9 +6,11 @@ import com.nju.storymapping.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 public class CardController {
@@ -50,10 +52,32 @@ public class CardController {
         return cardList;
     }
 
+    @PostMapping("/cardlistbydate")
+    public HashMap<String, Object> cardlistByDate() throws ParseException {
+
+        LinkedList<Integer> adddata = new LinkedList<>();
+        LinkedList<Integer> donedata = new LinkedList<>();
+        HashMap<String, Object> map = new HashMap<>();
+        Date dt = new Date();
+        Calendar rightNow = Calendar.getInstance();
+        rightNow.setTime(dt);
+        Date date = rightNow.getTime();
+        for (int i = 0; i < 10; i++) {
+            int addcount = cardRepository.countByDateAndStatusIsAdd(date);
+            int donecount = cardRepository.countByDateAndStatusIsDone(date);
+            adddata.add(addcount);
+            donedata.add(donecount);
+            rightNow.add(Calendar.DAY_OF_MONTH, -1);
+            date = rightNow.getTime();
+        }
+        map.put("adddt",adddata);
+        map.put("donedt",donedata);
+        return map;
+    }
+
     @GetMapping("/getCardIncharge")
     public List<Card> cardInChargeList(@RequestParam long id) {
         ArrayList<Card> cardList = cardService.getCardIncharge(id);
         return cardList;
-
     }
 }
--- src/main/java/com/nju/storymapping/respository/CardRepository.java
+++ src/main/java/com/nju/storymapping/respository/CardRepository.java
@@ -2,9 +2,24 @@ package com.nju.storymapping.respository;
 
 import com.nju.storymapping.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card,Long> {
 
+//    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Query("select count(c) from Card c where c.date <= ?1")
+    int countByDate(Date date);
+
+    @Query("select count(c) from Card c where c.date <= ?1 and c.status = 'end'")
+    int countByDateAndStatusIsDone(Date date);
+
+    @Query("select count(c) from Card c where c.date <= ?1 and c.status = 'add'")
+    int countByDateAndStatusIsAdd(Date date);
 }
